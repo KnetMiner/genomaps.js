e5360bb9f3d57153450a790ed8676135f91c777c
Mykola <mp@empat.tech>
2024-02-15 14:44:07 +0200

    basemap reader xml->json

--- dist/js/genomap.js
+++ dist/js/genomap.js
@@ -371,69 +371,13 @@ GENEMAP.AutoLayoutDecorator = function (userConfig) {
   };
 };
 
-// var GENEMAP = GENEMAP || {};
-
-// reads the chromosome data from the basemap file
-// GENEMAP.BasemapXmlReader = function () {
-//   var _readBasemapJSON = function (json) {
-//     var genome = {};
-//     genome.chromosomes = json?.genome?.chromosomes;
-//
-//     return genome;
-//   };
-//
-//   return {
-//     readBasemapXML: function (path) {
-//       log.info("reading basemap file: ", path);
-//       return d3.promise.json(path).then(_readBasemapJSON);
-//     },
-//   };
-// };
-
 var GENEMAP = GENEMAP || {};
 
 // reads the chromosome data from the basemap file
 GENEMAP.BasemapXmlReader = function () {
-  // read a chromosome band object from an XML element
-  var _readBand = function (elt) {
-    return {
-      index: elt.getAttribute("index"),
-      start: elt.getElementsByTagName("start")[0].childNodes[0].nodeValue,
-      end: elt.getElementsByTagName("end")[0].childNodes[0].nodeValue,
-      color: elt.getElementsByTagName("color")[0].childNodes[0].nodeValue,
-    };
-  };
-
-  // read a chromosome JS object from an XML element
-  var _readChromosome = function (elt) {
-    var chromosome = {
-      index: elt.getAttribute("index"),
-      length: elt.getAttribute("length"),
-      number: elt.getAttribute("number"),
-      bands: [],
-    };
-
-    var bandElements = elt.getElementsByTagName("band");
-
-    for (var j = 0; j < bandElements.length; j++) {
-      var band = _readBand(bandElements[j]);
-      chromosome.bands.push(band);
-    }
-
-    return chromosome;
-  };
-
-  // reads the genome data from a basemap XML document
-  var _readBasemapXML = function (xml) {
+  var _readBasemapJSON = function (json) {
     var genome = {};
-
-    genome.chromosomes = [];
-
-    var chromosomeElements = xml.getElementsByTagName("chromosome");
-    for (var i = 0; i < chromosomeElements.length; i++) {
-      var chromosome = _readChromosome(chromosomeElements[i]);
-      genome.chromosomes.push(chromosome);
-    }
+    genome.chromosomes = json?.chromosomes;
 
     return genome;
   };
@@ -441,17 +385,73 @@ GENEMAP.BasemapXmlReader = function () {
   return {
     readBasemapXML: function (path) {
       log.info("reading basemap file: ", path);
-      return d3.promise.xml(path).then(_readBasemapXML);
-    },
-
-    readBasemapXMLFromRawXML: function (xmlStr) {
-      log.info("reading basemap xml");
-      return new Promise(function (resolve, reject) {
-        resolve(new DOMParser().parseFromString(xmlStr, "application/xml"));
-      }).then(_readBasemapXML);
+      return d3.promise.json(path).then(_readBasemapJSON);
     },
   };
 };
+
+// var GENEMAP = GENEMAP || {};
+
+// // reads the chromosome data from the basemap file
+// GENEMAP.BasemapXmlReader = function () {
+//   // read a chromosome band object from an XML element
+//   var _readBand = function (elt) {
+//     return {
+//       index: elt.getAttribute("index"),
+//       start: elt.getElementsByTagName("start")[0].childNodes[0].nodeValue,
+//       end: elt.getElementsByTagName("end")[0].childNodes[0].nodeValue,
+//       color: elt.getElementsByTagName("color")[0].childNodes[0].nodeValue,
+//     };
+//   };
+
+//   // read a chromosome JS object from an XML element
+//   var _readChromosome = function (elt) {
+//     var chromosome = {
+//       index: elt.getAttribute("index"),
+//       length: elt.getAttribute("length"),
+//       number: elt.getAttribute("number"),
+//       bands: [],
+//     };
+
+//     var bandElements = elt.getElementsByTagName("band");
+
+//     for (var j = 0; j < bandElements.length; j++) {
+//       var band = _readBand(bandElements[j]);
+//       chromosome.bands.push(band);
+//     }
+
+//     return chromosome;
+//   };
+
+//   // reads the genome data from a basemap XML document
+//   var _readBasemapXML = function (xml) {
+//     var genome = {};
+
+//     genome.chromosomes = [];
+
+//     var chromosomeElements = xml.getElementsByTagName("chromosome");
+//     for (var i = 0; i < chromosomeElements.length; i++) {
+//       var chromosome = _readChromosome(chromosomeElements[i]);
+//       genome.chromosomes.push(chromosome);
+//     }
+
+//     return genome;
+//   };
+
+//   return {
+//     readBasemapXML: function (path) {
+//       log.info("reading basemap file: ", path);
+//       return d3.promise.xml(path).then(_readBasemapXML);
+//     },
+
+//     readBasemapXMLFromRawXML: function (xmlStr) {
+//       log.info("reading basemap xml");
+//       return new Promise(function (resolve, reject) {
+//         resolve(new DOMParser().parseFromString(xmlStr, "application/xml"));
+//       }).then(_readBasemapXML);
+//     },
+//   };
+// };
 
 //Source: https://github.com/gegham-khachatryan/BootstrapModalPopover
 //Forked from: https://github.com/scruffles/BootstrapModalPopover
@@ -2539,6 +2539,7 @@ GENEMAP.GeneMap = function (userConfig) {
 
   my.changeQtlColor = function (chromosomeId, color, label) {
     genome.chromosomes.forEach(function (chromosome) {
+      console.log('chromosome', chromosome)
       chromosome.layout.qtlNodes.forEach(function (qtlNode) {
         if (qtlNode.id === chromosomeId) {
           qtlNode.color = color;
